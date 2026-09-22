ddafc92a68d7bac1b8b5133667431e8fdbd63e02
Mika Flowers <pizzafan513@gmail.com>
2026-09-22 16:09:33 -0400

    Fix malformed book selection JSX callback

--- src/app/map/DevLibraryMap.tsx
+++ src/app/map/DevLibraryMap.tsx
@@ -679,7 +679,7 @@ export default function DevLibraryMap() {
               setReadingBook(null)
             })
           }
-        }
+        }}
         onFlightNavigationChange={setNavigation}
         onBackgroundClick={() => setSelectedId(null)}
         onProjectionChange={() => {}}
