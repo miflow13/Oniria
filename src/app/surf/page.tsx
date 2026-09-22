c2d4c48c504a252c56d182a47faca6c7ce610214
Mika Flowers <pizzafan513@gmail.com>
2026-09-22 05:50:21 -0400

    Add DEV WebSurf route

---
+++ src/app/surf/page.tsx
@@ -0,0 +1,10 @@
+import DevWebSurf from './DevWebSurf'
+
+export const metadata = {
+  title: 'DEV WebSurf',
+  description: 'Explore DEV Community as a first-person spatial website.',
+}
+
+export default function SurfPage() {
+  return <DevWebSurf />
+}
