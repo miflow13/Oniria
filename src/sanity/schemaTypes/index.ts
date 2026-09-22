cd2a9233c9d1d9e1d1ff54f646c4f8c81e9ba751
Mika Flowers <pizzafan513@gmail.com>
2026-09-22 16:53:13 -0400

    Register archive world schemas

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,4 +1,15 @@
+import {archiveJourneyType} from './archiveJourneyType'
+import {curatedArticleType} from './curatedArticleType'
 import {dreamType} from './dreamType'
+import {libraryConfigType} from './libraryConfigType'
+import {libraryDistrictType} from './libraryDistrictType'
 import {symbolType} from './symbolType'
 
-export const schemaTypes = [dreamType, symbolType]
+export const schemaTypes = [
+  libraryConfigType,
+  libraryDistrictType,
+  curatedArticleType,
+  archiveJourneyType,
+  dreamType,
+  symbolType,
+]
