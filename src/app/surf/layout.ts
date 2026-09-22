96b230041ec7dc79515fff3a8599895c8055e6d7
Mika Flowers <pizzafan513@gmail.com>
2026-09-22 13:24:41 -0400

    Add DEV layout types and shelf keys

---
+++ src/app/surf/layout.ts
@@ -0,0 +1,26 @@
+import rawLayout from './layout.json'
+
+export type ShelfLayoutTransform = {
+  x: number
+  z: number
+  rotationY: number
+}
+
+export type SurfLayoutConfig = {
+  version: 1
+  shelves: Record<string, ShelfLayoutTransform>
+}
+
+export const surfLayout = rawLayout as SurfLayoutConfig
+
+export function shelfLayoutKey(
+  shelfKey: string | undefined,
+  floorIndex = 0,
+) {
+  if (!shelfKey) return null
+  return (
+    floorIndex +
+    ':' +
+    shelfKey.replace(/:level-\d+$/, '')
+  )
+}
