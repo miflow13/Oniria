4c1bbe657488f74c95e13ce72ae0892473c427ff
Mika Flowers <pizzafan513@gmail.com>
2026-09-23 16:45:47 -0400

    Size DEV shelf capacity to normalized surveyed layout

--- src/app/map/DevLibraryMap.tsx
+++ src/app/map/DevLibraryMap.tsx
@@ -31,7 +31,7 @@ import type {
 import styles from './library.module.css'
 import {
   hallwayShelfPlacements,
-  LIBRARY_ROOM_SHELF_SLOTS,
+  LIBRARY_MAX_ROOM_SHELF_COUNT,
   roomShelfPlacements,
   type RoomShelfPlacement,
 } from './libraryRoomLayout'
@@ -41,7 +41,7 @@ const QUALITY: DreamQuality = 'cinematic'
 const CATALOG_PAGE_SIZE = 100
 const CATALOG_BOOKS_PER_SHELF = 9
 const DISTRICT_RENDERED_SHELF_LIMIT =
-  LIBRARY_ROOM_SHELF_SLOTS.length
+  LIBRARY_MAX_ROOM_SHELF_COUNT
 const DISTRICT_VISIBLE_ARTICLE_CAPACITY =
   DISTRICT_RENDERED_SHELF_LIMIT * CATALOG_BOOKS_PER_SHELF * 2
 const DISTRICT_SHELF_PAIR_OFFSETS = [-.68, .68] as const
