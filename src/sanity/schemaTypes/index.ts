34d1a3c833ff0d5575b87009737362cd7c33f072
Mika Flowers <pizzafan513@gmail.com>
2026-09-23 15:53:45 -0400

    Register library layout pin schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,11 +3,13 @@ import {curatedArticleType} from './curatedArticleType'
 import {dreamType} from './dreamType'
 import {libraryConfigType} from './libraryConfigType'
 import {libraryDistrictType} from './libraryDistrictType'
+import {libraryLayoutMarkerType} from './libraryLayoutMarkerType'
 import {symbolType} from './symbolType'
 
 export const schemaTypes = [
   libraryConfigType,
   libraryDistrictType,
+  libraryLayoutMarkerType,
   curatedArticleType,
   archiveJourneyType,
   dreamType,
