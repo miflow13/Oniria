e31c0d447e30d59ff371ba4d5b1f0a7d3a9aeea2
Mika Flowers <pizzafan513@gmail.com>
2026-09-24 01:59:45 -0400

    Register living library slot schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,12 +4,14 @@ import {dreamType} from './dreamType'
 import {libraryConfigType} from './libraryConfigType'
 import {libraryDistrictType} from './libraryDistrictType'
 import {libraryLayoutMarkerType} from './libraryLayoutMarkerType'
+import {librarySlotStateType} from './librarySlotStateType'
 import {symbolType} from './symbolType'
 
 export const schemaTypes = [
   libraryConfigType,
   libraryDistrictType,
   libraryLayoutMarkerType,
+  librarySlotStateType,
   curatedArticleType,
   archiveJourneyType,
   dreamType,
